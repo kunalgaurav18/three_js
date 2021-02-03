c82ea5f9241a1eb30c1e6eba5ac685f51447f194
sumitsharmamanit <73555793+sumitsharmamanit@users.noreply.github.com>
2021-02-03 15:17:25 +0000

    camera position changes

--- js/obj_reader.js
+++ js/obj_reader.js
@@ -6,45 +6,48 @@ import { MTLLoader } from "https://threejsfundamentals.org/threejs/resources/thr
 function main() {
   const mtlFile = "obj/sonic/sonic.mtl";
   const objFile = "obj/sonic/sonic.obj";
+
+  // const mtlFile = "obj/model_1/model_1.mtl";
+  // const objFile = "obj/model_1/model_1.obj";
+
   const canvas = document.querySelector("#c");
   const renderer = new THREE.WebGLRenderer({ canvas });
 
   const fov = 45;
   const aspect = 2; // the canvas default
   const near = 0.1;
-  const far = 100;
+  const far = 1000;
   const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
   camera.position.set(0, 10, 20);
 
   const controls = new OrbitControls(camera, canvas);
-  controls.target.set(0, 5, 0);
+  controls.target.set(0, 0, 0); //sets initial location of the object
   controls.update();
 
   const scene = new THREE.Scene();
   scene.background = new THREE.Color("white");
 
-  {
-    const planeSize = 40;
+  // {
+  //   const planeSize = 40;
+  //   const loader = new THREE.TextureLoader();
+  //   const texture = loader.load(
+  //     "https://threejsfundamentals.org/threejs/resources/images/checker.png"
+  //   );
+  //   texture.wrapS = THREE.RepeatWrapping;
+  //   texture.wrapT = THREE.RepeatWrapping;
+  //   texture.magFilter = THREE.NearestFilter;
+  //   const repeats = planeSize / 2;
+  //   texture.repeat.set(repeats, repeats);
 
-    const loader = new THREE.TextureLoader();
-    const texture = loader.load(
-      "https://threejsfundamentals.org/threejs/resources/images/checker.png"
-    );
-    texture.wrapS = THREE.RepeatWrapping;
-    texture.wrapT = THREE.RepeatWrapping;
-    texture.magFilter = THREE.NearestFilter;
-    const repeats = planeSize / 2;
-    texture.repeat.set(repeats, repeats);
-
-    const planeGeo = new THREE.PlaneBufferGeometry(planeSize, planeSize);
-    const planeMat = new THREE.MeshPhongMaterial({
-      map: texture,
-      side: THREE.DoubleSide,
-    });
-    const mesh = new THREE.Mesh(planeGeo, planeMat);
-    mesh.rotation.x = Math.PI * -0.5;
-    // scene.add(mesh);
-  }
+  //   const planeGeo = new THREE.PlaneBufferGeometry(planeSize, planeSize);
+  //   const planeMat = new THREE.MeshPhongMaterial({
+  //     map: texture,
+  //     side: THREE.DoubleSide,
+  //   });
+  //   const mesh = new THREE.Mesh(planeGeo, planeMat);
+  //   mesh.rotation.x = Math.PI * -0.5;
+  //   scene.add(mesh);
+  // }
 
   {
     const skyColor = 0xb1e1ff; // light blue
